47cb3569859db16abcfa9cc7156a7de11b35ab97
John Haver Herrera <johnhaverh@gmail.com>
2023-08-28 15:41:31 -0500

    implements provider & refactor counter_provider_page

--- lib/ui/pages/counter_provider_page.dart
+++ lib/ui/pages/counter_provider_page.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:bases_web/provider/counter_provider.dart';
 import 'package:bases_web/ui/shared/custom_app_menu.dart';
 import 'package:bases_web/ui/shared/custom_flat_button.dart';
 
-class CounterProviderPage extends StatefulWidget {
-  const CounterProviderPage({Key? key}) : super(key: key);
+class CounterProviderPage extends StatelessWidget {
+  const CounterProviderPage({super.key});
 
-  @override
-  State<CounterProviderPage> createState() => _CounterProviderPageState();
-}
-
-class _CounterProviderPageState extends State<CounterProviderPage> {
-  int counter=15;
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: ( _) => CounterProvider(),
+      child: _CounterProviderPageBody()
+    );
+  }
+}
+
+class _CounterProviderPageBody extends StatelessWidget {
+  const _CounterProviderPageBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final counterProvider = Provider.of<CounterProvider>(context);
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -25,15 +39,15 @@ class _CounterProviderPageState extends State<CounterProviderPage> {
             fit: BoxFit.contain,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text("Contador: $counter ",style: const TextStyle(fontSize: 80, fontWeight: FontWeight.bold)),
+              child: Text("Contador: ${ counterProvider.counter } ",style: const TextStyle(fontSize: 80, fontWeight: FontWeight.bold)),
             )
           ),
           
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CustomFlatButton(text: "Incrementar", onPressed: () => setState(()=> counter++)),
-              CustomFlatButton(text: "Decrementar", onPressed: () => setState(()=> counter--)),      
+              CustomFlatButton(text: "Incrementar", onPressed: () => counterProvider.increment()),
+              CustomFlatButton(text: "Decrementar", onPressed: () => counterProvider.decrement()),      
             ],
           ),
           const Spacer(),
